aa55bde4ef35a3a20120892973ad06e456a9c5fc
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-06-26 18:12:39 +0530

    Adedd Youtube Trailer iFrame

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,35 +1,38 @@
 <template>
-  <div :style='{backgroundImage: movie.largeImgSrc}' :class="{'films-bg': true, favorite: movie.favorite}"> 
-    <div class="header">
-        <i class="material-icons" style="font-size:48px;color:red;cursor: pointer; margin:auto 0">menu</i>
-        <div class="nav">
-            <div v-for="tab in tabs" :class="{selected: selectedTab==tab}" v-on:click="handleTab(tab)">{{tab}}</div>
+<div :style='{backgroundImage: movie.largeImgSrc}' :class="{'films-bg': true, favorite: movie.favorite}">
+    <iframe :src="movie.trailerPath" style="width:100%; height:100%" frameborder="0" allowfullscreen v-if="play"></iframe>
+    <div v-else> 
+        <div class="header">
+            <i class="material-icons" style="font-size:48px;color:red;cursor: pointer; margin:auto 0">menu</i>
+            <div class="nav">
+                <div v-for="tab in tabs" :class="{selected: selectedTab==tab}" v-on:click="handleTab(tab)">{{tab}}</div>
+            </div>
+            <div style="color:red;margin: auto 10px;font-weight:bold;cursor: pointer;" v-on:click="goHome">VUEFLIX</div>
         </div>
-        <div style="color:red;margin: auto 10px;font-weight:bold;cursor: pointer;">VUEFLIX</div>
-    </div>
 
-    <div style="text-align:left;margin: 5px 40px; line-height:1.2; width:45%">
-        <p>
-            <span style="color:white; font-size:40px; font-weight:bold">{{movie.title}}</span><br>
-            <span style="color:red; font-weight:bold">{{movie.duration}} | {{movie.genre}} | {{movie.releaseDate}}</span>
-        </p>
-        <p style="color:white;line-height:1.5">{{movie.description}}</p>
-        <div style="display:flex">
-            <div style="display:flex; cursor:pointer; background-color:#DE0000;color:white; border-radius: 500px; border:none; width:fit-content; padding:5px 12px 5px 16px ">
-                <span style="display:block;margin-top:3px;font-weight:bold;margin-right:2px">Play</span> 
-                <i class="material-icons" style="font-size:24p; margin:auto 0">play_arrow</i>
-            </div>
-            <div v-if="movie.favorite" class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
-                <span style="margin:4px">Remove from favorites</span>
-                <i class="material-icons" style="font-size:18px;margin: auto 4px;">remove_circle_outline</i>
-            </div>
-            <div v-else class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
-                <span style="margin:4px">Add to favorites</span>
-                <i class="material-icons" style="font-size:18px;margin: auto 4px;">add_circle_outline</i>
+        <div style="text-align:left;margin: 5px 40px; line-height:1.2; width:45%">
+            <p>
+                <span style="color:white; font-size:40px; font-weight:bold">{{movie.title}}</span><br>
+                <span style="color:red; font-weight:bold">{{movie.duration}} | {{movie.genre}} | {{movie.releaseDate}}</span>
+            </p>
+            <p style="color:white;line-height:1.5">{{movie.description}}</p>
+            <div style="display:flex">
+                <div style="display:flex; cursor:pointer; background-color:#DE0000;color:white; border-radius: 500px; border:none; width:fit-content; padding:5px 12px 5px 16px" v-on:click="$emit('togglePlay')">
+                    <span style="display:block;margin-top:3px;font-weight:bold;margin-right:2px">Play</span> 
+                    <i class="material-icons" style="font-size:24p; margin:auto 0">play_arrow</i>
+                </div>
+                <div v-if="movie.favorite" class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
+                    <span style="margin:4px">Remove from favorites</span>
+                    <i class="material-icons" style="font-size:18px;margin: auto 4px;">remove_circle_outline</i>
+                </div>
+                <div v-else class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
+                    <span style="margin:4px">Add to favorites</span>
+                    <i class="material-icons" style="font-size:18px;margin: auto 4px;">add_circle_outline</i>
+                </div>
             </div>
         </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -39,7 +42,12 @@ export default {
       movie: {
         type: Object,
         required: true
-      }
+      },
+      
+      play: {
+          type: Boolean,
+          required: true
+      },
   },
   data() {
       return {
@@ -49,7 +57,7 @@ export default {
               "Films",
               "Shows",
               "Music"
-          ]
+          ],
       };
   },
   methods: {
@@ -109,6 +117,7 @@ export default {
         /* Important for applying gradient to background images*/
         background-color: rgba(0, 0, 0, 0.7);
         background-blend-mode: multiply;
+        padding:0px;
     }
 
     .nav {
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,9 @@
           <span style="color:#FF0000;font-size:64px;">VUEFLIX</span>
           <span style="color:#fff; font-size:20px;">Select a movie below from the list of critically acclaimed <br>Christopher Nolan films.</span>
       </div>
-      <films class= "header" v-else :movie="movies[movieId]" @toggleFavorite="toggleFavorite" @goHome = "movieId=''"></films>
+      <films class= "header" v-else :movie="movies[movieId]" :play="play" @togglePlay="play=!play" @toggleFavorite="toggleFavorite" @goHome = "movieId=''"></films>
       <section id="footer">
-          <div v-for="movie in movies" v-on:click="movieId=movie.id" style="flex:20%;padding:0px 10px; position: relative">
+          <div v-for="movie in movies" v-on:click="handleMovieClick(movie.id)" style="flex:20%;padding:0px 10px; position: relative">
             <img :src="movie.smallImgSrc" alt="placeholder" style="width:90%;">
             <i v-if="movie.favorite" class="material-icons" style="position:absolute; right:10%; top: 2px; color:yellow; font-size:18px" >check_circle</i>
           </div>
@@ -24,77 +24,83 @@ export default {
   },
   data: () => ({
       movieId: "",
-     movies :  {
-  "dunkirk": {
-    "id": 'dunkirk',
-    "title": "Dunkirk",
-    "subtitle": "Dunkirk",
-    "description": `Miraculous evacuation of Allied soldiers from Belgium, Britain, Canada, and France, who were cut off and surrounded by the German army from the beaches and harbor of Dunkirk, France, during the Battle of France in World War II.`,
-    "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/fudEG1VUWuOqleXv6NwCExK0VLy.jpg')`,
-    "smallImgSrc": 'https://image.tmdb.org/t/p/w185/fudEG1VUWuOqleXv6NwCExK0VLy.jpg',
-    "releaseDate": 'July 21 2017',
-    "duration": '1hr 46min',
-    "genre": 'Action, Drama, History',
-    "trailerPath": 'https://www.youtube.com/embed/F-eMt3SrfFU',
-    "favorite": false
-  },
-  "interstellar": {
-    "id": 'interstellar',
-    "title": "Interstellar",
-    "subtitle": "Interstellar",
-    "description": `Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.`,
-    "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg')`,
-    "smallImgSrc": 'https://image.tmdb.org/t/p/w185/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg',
-    "releaseDate": 'November 7 2014',
-    "duration": '2hr 49min',
-    "genre": 'Adventure, Drama',
-    "trailerPath": 'https://www.youtube.com/embed/zSWdZVtXT7E',
-    "favorite": false
-  },
-  "the-dark-knight-rises": {
-    "id": 'the-dark-knight-rises',
-    "title": "The Dark Knight Rises",
-    "subtitle": "TDKR",
-    "description": `Batman encounters the mysterious Selina Kyle and the villainous Bane, a new terrorist leader who overwhelms Gotham's finest. The Dark Knight resurfaces to protect a city that has branded him an enemy.`,
-    "smallImgSrc": 'https://image.tmdb.org/t/p/w185/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg',
-    "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg')`,
-    "releaseDate": 'July 20 2012',
-    "duration": '2hr 44min',
-    "genre": 'Action, Thriller',
-    "trailerPath": 'https://www.youtube.com/embed/g8evyE9TuYk',
-    "favorite": false
-  },
-  "inception": {
-    "id": 'inception',
-    "title": "Inception",
-    "subtitle": "Inception",
-    "description": `Cobb, a skilled thief is offered a chance to regain his old life as payment for a task considered to be impossible: \"inception\", the implantation of another person's idea into a target's subconscious.`,
-    "smallImgSrc": 'https://image.tmdb.org/t/p/w185/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg',
-    "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg')`,
-    "releaseDate": 'July 10 2010',
-    "duration": '2hr 28min',
-    "genre": 'Action, Adventure, Sci-Fi',
-    "trailerPath": 'https://www.youtube.com/embed/8hP9D6kZseM',
-    "favorite": false
-  },
-  "the-prestige": {
-    "id": 'the-prestige',
-    "title": "The Prestige",
-    "subtitle": "Prestige",
-    "description": `A mysterious story of two magicians whose intense rivalry leads them on a life-long battle for supremacy - to create the ultimate illusion whilst sacrificing everything they have to outwit the other.`,
-    "smallImgSrc": 'https://image.tmdb.org/t/p/w185/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg',
-    "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg')`,
-    "releaseDate": 'October 20 2006',
-    "duration": '2hr 10min',
-    "genre": 'Drama, Mystery, Sci-Fi',
-    "trailerPath": 'https://www.youtube.com/embed/ijXruSzfGEc',
-    "favorite": false
-  }
+      play:false,
+      movies :  {
+        "dunkirk": {
+            "id": 'dunkirk',
+            "title": "Dunkirk",
+            "subtitle": "Dunkirk",
+            "description": `Miraculous evacuation of Allied soldiers from Belgium, Britain, Canada, and France, who were cut off and surrounded by the German army from the beaches and harbor of Dunkirk, France, during the Battle of France in World War II.`,
+            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/fudEG1VUWuOqleXv6NwCExK0VLy.jpg')`,
+            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/fudEG1VUWuOqleXv6NwCExK0VLy.jpg',
+            "releaseDate": 'July 21 2017',
+            "duration": '1hr 46min',
+            "genre": 'Action, Drama, History',
+            "trailerPath": 'https://www.youtube.com/embed/F-eMt3SrfFU',
+            "favorite": false
+        },
+        "interstellar": {
+            "id": 'interstellar',
+            "title": "Interstellar",
+            "subtitle": "Interstellar",
+            "description": `Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.`,
+            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg')`,
+            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg',
+            "releaseDate": 'November 7 2014',
+            "duration": '2hr 49min',
+            "genre": 'Adventure, Drama',
+            "trailerPath": 'https://www.youtube.com/embed/zSWdZVtXT7E',
+            "favorite": false
+        },
+        "the-dark-knight-rises": {
+            "id": 'the-dark-knight-rises',
+            "title": "The Dark Knight Rises",
+            "subtitle": "TDKR",
+            "description": `Batman encounters the mysterious Selina Kyle and the villainous Bane, a new terrorist leader who overwhelms Gotham's finest. The Dark Knight resurfaces to protect a city that has branded him an enemy.`,
+            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg',
+            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg')`,
+            "releaseDate": 'July 20 2012',
+            "duration": '2hr 44min',
+            "genre": 'Action, Thriller',
+            "trailerPath": 'https://www.youtube.com/embed/g8evyE9TuYk',
+            "favorite": false
+        },
+        "inception": {
+            "id": 'inception',
+            "title": "Inception",
+            "subtitle": "Inception",
+            "description": `Cobb, a skilled thief is offered a chance to regain his old life as payment for a task considered to be impossible: \"inception\", the implantation of another person's idea into a target's subconscious.`,
+            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg',
+            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg')`,
+            "releaseDate": 'July 10 2010',
+            "duration": '2hr 28min',
+            "genre": 'Action, Adventure, Sci-Fi',
+            "trailerPath": 'https://www.youtube.com/embed/8hP9D6kZseM',
+            "favorite": false
+        },
+        "the-prestige": {
+            "id": 'the-prestige',
+            "title": "The Prestige",
+            "subtitle": "Prestige",
+            "description": `A mysterious story of two magicians whose intense rivalry leads them on a life-long battle for supremacy - to create the ultimate illusion whilst sacrificing everything they have to outwit the other.`,
+            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg',
+            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg')`,
+            "releaseDate": 'October 20 2006',
+            "duration": '2hr 10min',
+            "genre": 'Drama, Mystery, Sci-Fi',
+            "trailerPath": 'https://www.youtube.com/embed/ijXruSzfGEc',
+            "favorite": false
+        }
 }
   }),
   methods: {
       toggleFavorite(movieId) {
           this.movies[movieId].favorite  = !this.movies[movieId].favorite;
+      },
+
+      handleMovieClick(movieId) {
+          this.movieId = movieId;
+          this.play = false;
       }
   }
 }
