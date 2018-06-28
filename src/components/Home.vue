258ba9a36453096f847d0537e8febce7d92d3ae2
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-06-28 15:33:27 +0530

    Added TheMovieDb API call

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,8 @@
       </div>
       <films class= "header" v-else :movie="movies[movieId]" :play="play" @togglePlay="play=!play" @toggleFavorite="toggleFavorite" @goHome = "movieId=''"></films>
       <section id="footer">
-          <div v-for="movie in movies" v-on:click="handleMovieClick(movie.id)" style="flex:20%;padding:0px 10px; position: relative">
+          <div v-if="loading" style="color:white;font-size:24px">Loading...</div>
+          <div v-else v-for="movie in movies" v-on:click="handleMovieClick(movie.id)" style="padding:0px 10px; position: relative">
             <img :src="movie.smallImgSrc" alt="placeholder" style="width:90%;">
             <i v-if="movie.favorite" class="material-icons" style="position:absolute; right:10%; top: 2px; color:yellow; font-size:18px" >check_circle</i>
           </div>
@@ -16,6 +17,8 @@
 
 <script>
 import Films from './Films'
+import axios from 'axios'
+import _ from 'lodash'
 
 export default {
   name: 'Home',
@@ -25,74 +28,109 @@ export default {
   data: () => ({
       movieId: "",
       play:false,
-      movies :  {
-        "dunkirk": {
-            "id": 'dunkirk',
-            "title": "Dunkirk",
-            "subtitle": "Dunkirk",
-            "description": `Miraculous evacuation of Allied soldiers from Belgium, Britain, Canada, and France, who were cut off and surrounded by the German army from the beaches and harbor of Dunkirk, France, during the Battle of France in World War II.`,
-            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/fudEG1VUWuOqleXv6NwCExK0VLy.jpg')`,
-            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/fudEG1VUWuOqleXv6NwCExK0VLy.jpg',
-            "releaseDate": 'July 21 2017',
-            "duration": '1hr 46min',
-            "genre": 'Action, Drama, History',
-            "trailerPath": 'https://www.youtube.com/embed/F-eMt3SrfFU',
-            "favorite": false
-        },
-        "interstellar": {
-            "id": 'interstellar',
-            "title": "Interstellar",
-            "subtitle": "Interstellar",
-            "description": `Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.`,
-            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg')`,
-            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg',
-            "releaseDate": 'November 7 2014',
-            "duration": '2hr 49min',
-            "genre": 'Adventure, Drama',
-            "trailerPath": 'https://www.youtube.com/embed/zSWdZVtXT7E',
-            "favorite": false
-        },
-        "the-dark-knight-rises": {
-            "id": 'the-dark-knight-rises',
-            "title": "The Dark Knight Rises",
-            "subtitle": "TDKR",
-            "description": `Batman encounters the mysterious Selina Kyle and the villainous Bane, a new terrorist leader who overwhelms Gotham's finest. The Dark Knight resurfaces to protect a city that has branded him an enemy.`,
-            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg',
-            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg')`,
-            "releaseDate": 'July 20 2012',
-            "duration": '2hr 44min',
-            "genre": 'Action, Thriller',
-            "trailerPath": 'https://www.youtube.com/embed/g8evyE9TuYk',
-            "favorite": false
-        },
-        "inception": {
-            "id": 'inception',
-            "title": "Inception",
-            "subtitle": "Inception",
-            "description": `Cobb, a skilled thief is offered a chance to regain his old life as payment for a task considered to be impossible: \"inception\", the implantation of another person's idea into a target's subconscious.`,
-            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg',
-            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg')`,
-            "releaseDate": 'July 10 2010',
-            "duration": '2hr 28min',
-            "genre": 'Action, Adventure, Sci-Fi',
-            "trailerPath": 'https://www.youtube.com/embed/8hP9D6kZseM',
-            "favorite": false
-        },
-        "the-prestige": {
-            "id": 'the-prestige',
-            "title": "The Prestige",
-            "subtitle": "Prestige",
-            "description": `A mysterious story of two magicians whose intense rivalry leads them on a life-long battle for supremacy - to create the ultimate illusion whilst sacrificing everything they have to outwit the other.`,
-            "smallImgSrc": 'https://image.tmdb.org/t/p/w185/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg',
-            "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg')`,
-            "releaseDate": 'October 20 2006',
-            "duration": '2hr 10min',
-            "genre": 'Drama, Mystery, Sci-Fi',
-            "trailerPath": 'https://www.youtube.com/embed/ijXruSzfGEc',
-            "favorite": false
-        }
-}
+      loading: true,
+      movies: {},
+    //   movies :  {
+    //     "dunkirk": {
+    //         "id": 'dunkirk',
+    //         "title": "Dunkirk",
+    //         "subtitle": "Dunkirk",
+    //         "description": `Miraculous evacuation of Allied soldiers from Belgium, Britain, Canada, and France, who were cut off and surrounded by the German army from the beaches and harbor of Dunkirk, France, during the Battle of France in World War II.`,
+    //         "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/fudEG1VUWuOqleXv6NwCExK0VLy.jpg')`,
+    //         "smallImgSrc": 'https://image.tmdb.org/t/p/w185/fudEG1VUWuOqleXv6NwCExK0VLy.jpg',
+    //         "releaseDate": 'July 21 2017',
+    //         "duration": '1hr 46min',
+    //         "genre": 'Action, Drama, History',
+    //         "trailerPath": 'https://www.youtube.com/embed/F-eMt3SrfFU',
+    //         "favorite": false
+    //     },
+    //     "interstellar": {
+    //         "id": 'interstellar',
+    //         "title": "Interstellar",
+    //         "subtitle": "Interstellar",
+    //         "description": `Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.`,
+    //         "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg')`,
+    //         "smallImgSrc": 'https://image.tmdb.org/t/p/w185/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg',
+    //         "releaseDate": 'November 7 2014',
+    //         "duration": '2hr 49min',
+    //         "genre": 'Adventure, Drama',
+    //         "trailerPath": 'https://www.youtube.com/embed/zSWdZVtXT7E',
+    //         "favorite": false
+    //     },
+    //     "the-dark-knight-rises": {
+    //         "id": 'the-dark-knight-rises',
+    //         "title": "The Dark Knight Rises",
+    //         "subtitle": "TDKR",
+    //         "description": `Batman encounters the mysterious Selina Kyle and the villainous Bane, a new terrorist leader who overwhelms Gotham's finest. The Dark Knight resurfaces to protect a city that has branded him an enemy.`,
+    //         "smallImgSrc": 'https://image.tmdb.org/t/p/w185/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg',
+    //         "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/3bgtUfKQKNi3nJsAB5URpP2wdRt.jpg')`,
+    //         "releaseDate": 'July 20 2012',
+    //         "duration": '2hr 44min',
+    //         "genre": 'Action, Thriller',
+    //         "trailerPath": 'https://www.youtube.com/embed/g8evyE9TuYk',
+    //         "favorite": false
+    //     },
+    //     "inception": {
+    //         "id": 'inception',
+    //         "title": "Inception",
+    //         "subtitle": "Inception",
+    //         "description": `Cobb, a skilled thief is offered a chance to regain his old life as payment for a task considered to be impossible: \"inception\", the implantation of another person's idea into a target's subconscious.`,
+    //         "smallImgSrc": 'https://image.tmdb.org/t/p/w185/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg',
+    //         "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg')`,
+    //         "releaseDate": 'July 10 2010',
+    //         "duration": '2hr 28min',
+    //         "genre": 'Action, Adventure, Sci-Fi',
+    //         "trailerPath": 'https://www.youtube.com/embed/8hP9D6kZseM',
+    //         "favorite": false
+    //     },
+    //     "the-prestige": {
+    //         "id": 'the-prestige',
+    //         "title": "The Prestige",
+    //         "subtitle": "Prestige",
+    //         "description": `A mysterious story of two magicians whose intense rivalry leads them on a life-long battle for supremacy - to create the ultimate illusion whilst sacrificing everything they have to outwit the other.`,
+    //         "smallImgSrc": 'https://image.tmdb.org/t/p/w185/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg',
+    //         "largeImgSrc": `url('https://image.tmdb.org/t/p/w780/c5o7FN2vzI7xlU6IF1y64mgcH9E.jpg')`,
+    //         "releaseDate": 'October 20 2006',
+    //         "duration": '2hr 10min',
+    //         "genre": 'Drama, Mystery, Sci-Fi',
+    //         "trailerPath": 'https://www.youtube.com/embed/ijXruSzfGEc',
+    //         "favorite": false
+    //     }
+// }
   }),
+  created () {
+        const API_KEY = "ab5a7db6d911744f7e4e910d9eb5582c";
+        const BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+        const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+        const BASE_YOUTUBE_TRAILER_URL = "https://www.youtube.com/watch?v=GTJr8OvyEVQ";
+
+        axios.get(BASE_URL, {
+            params : {
+                api_key : API_KEY,
+                language : "en-US",
+                sort_by: "popularity.desc",
+                include_adult: false,
+                page: 1
+            }
+
+        }).then(response => {
+            _.forEach(response.data.results, (movie, index) => {
+                this.movies[movie.id] = {
+                    id: movie.id,
+                    title: movie.title,
+                    description: movie.overview,
+                    largeImgSrc: BASE_IMAGE_URL+movie.poster_path,
+                    smallImgSrc: BASE_IMAGE_URL+movie.poster_path,
+                    releaseDate: movie.release_date,
+                    genre: movie.genre_ids,
+                    favorite: false
+                };
+                if (index > 5) 
+                    return false;
+            });
+                this.loading = false;
+        });
+  },
   methods: {
       toggleFavorite(movieId) {
           this.movies[movieId].favorite  = !this.movies[movieId].favorite;
@@ -133,7 +171,7 @@ export default {
 
     #footer {
         display:flex;
-        flex: 28%;
+        flex: 35%;
         justify-content:space-around; 
         align-items:center;       
         background-color: #A3081E;
@@ -143,7 +181,7 @@ export default {
     .header {
         display:flex;
         flex-direction: column;
-        flex: 72%;
+        flex: 65%;
         background-color:#1F1D1D;
         align-items:center;
         justify-content:center;
