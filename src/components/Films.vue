2c10c1d41904fc22208b6bd97c20bd3bc066f9c2
Shahbaz Hussain <shahbaz.h96@gmail.com>
2019-09-01 22:32:01 +0530

    Fix linter warnings

--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,280 +1,313 @@
 <template>
-<div :style= '{backgroundImage: `url(${movie.largeImgSrc})`}' :class="{'films-bg': true, favorite: movie.favorite}">
-    <div class=content>    
-        <div v-if="play" class="iframe-loader">
-            <iframe :src="trailerPath" style="width:100%; height:100%" frameborder="0" allowfullscreen></iframe>
+  <div
+    :style="{backgroundImage: `url(${movie.largeImgSrc})`}"
+    :class="{'films-bg': true, favorite: movie.favorite}"
+  >
+    <div class="content">
+      <div v-if="play" class="iframe-loader">
+        <iframe :src="trailerPath" style="width:100%; height:100%" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div style="width:100%" v-else>
+        <div class="header">
+          <i
+            class="material-icons"
+            style="font-size:48px;color:red;cursor: pointer; margin:auto 0"
+          >menu</i>
+          <div class="nav">
+            <div
+              v-for="tab in tabs"
+              v-bind:key="tab"
+              :class="{selected: selectedTab==tab}"
+              v-on:click="handleTab(tab)"
+            >{{tab}}</div>
+          </div>
+          <div
+            style="color:red;margin: auto 10px;font-weight:bold;cursor: pointer;"
+            v-on:click="goHome"
+          >VUEFLIX</div>
         </div>
 
-        <div style="width:100%" v-else> 
-            
-            <div class="header">
-                <i class="material-icons" style="font-size:48px;color:red;cursor: pointer; margin:auto 0">menu</i>
-                <div class="nav">
-                    <div v-for="tab in tabs" :class="{selected: selectedTab==tab}" v-on:click="handleTab(tab)">{{tab}}</div>
-                </div>
-                <div style="color:red;margin: auto 10px;font-weight:bold;cursor: pointer;" v-on:click="goHome">VUEFLIX</div>
+        <div style="height:100%; width:100%; display:flex; flex-direction:row;">
+          <div
+            style="text-align:left;margin: 5px 40px; line-height:1.2; width:65%; justfy-content:space-around"
+          >
+            <p>
+              <a
+                style="color:white; font-size:40px; font-weight:bold"
+                :href="homepage"
+                id="title"
+                target="_blank"
+              >{{movie.title}}</a>
+              <br />
+              <span
+                style="color:red; font-weight:bold"
+              >{{this.runtime}} | {{this.genre}} | {{movie.releaseDate}}</span>
+            </p>
+
+            <p style="color:white;line-height:1.5" class="fixed-line">{{movie.description}}</p>
+
+            <div style="display:flex">
+              <div
+                style="display:flex; cursor:pointer; background-color:#DE0000;color:white; border-radius: 500px; border:none; width:fit-content; padding:5px 12px 5px 16px"
+                v-on:click="fetchYoutubeURL"
+              >
+                <span style="display:block;margin-top:3px;font-weight:bold;margin-right:2px">Play</span>
+                <i class="material-icons" style="font-size:24p; margin:auto 0">play_arrow</i>
+              </div>
+
+              <div
+                v-if="movie.favorite"
+                class="fav"
+                style="display:flex;cursor:pointer"
+                v-on:click="toggleFav"
+              >
+                <span style="margin:4px">Remove from favorites</span>
+                <i
+                  class="material-icons"
+                  style="font-size:18px;margin: auto 4px;"
+                >remove_circle_outline</i>
+              </div>
+
+              <div v-else class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
+                <span style="margin:4px">Add to favorites</span>
+                <i
+                  class="material-icons"
+                  style="font-size:18px;margin: auto 4px;"
+                >add_circle_outline</i>
+              </div>
             </div>
+          </div>
 
-            <div style="height:100%; width:100%; display:flex; flex-direction:row;">
-
-                <div style="text-align:left;margin: 5px 40px; line-height:1.2; width:65%; justfy-content:space-around">
-                    
-                    <p>
-                        <a style="color:white; font-size:40px; font-weight:bold" :href="homepage" id="title" target="_blank">{{movie.title}}</a><br>
-                        <span style="color:red; font-weight:bold">{{this.runtime}} | {{this.genre}} | {{movie.releaseDate}}</span>
-                    </p>
-
-                    <p style="color:white;line-height:1.5" class=fixed-line>{{movie.description}}</p>
-                    
-                    <div style="display:flex">
-                        
-                        <div style="display:flex; cursor:pointer; background-color:#DE0000;color:white; border-radius: 500px; border:none; width:fit-content; padding:5px 12px 5px 16px" v-on:click="fetchYoutubeURL">
-                            <span style="display:block;margin-top:3px;font-weight:bold;margin-right:2px">Play</span> 
-                            <i class="material-icons" style="font-size:24p; margin:auto 0">play_arrow</i>
-                        
-                        </div>
-                        
-                        <div v-if="movie.favorite" class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
-                            <span style="margin:4px">Remove from favorites</span>
-                            <i class="material-icons" style="font-size:18px;margin: auto 4px;">remove_circle_outline</i>
-                        </div>
-
-                        <div v-else class="fav" style="display:flex;cursor:pointer" v-on:click="toggleFav">
-                            <span style="margin:4px">Add to favorites</span>
-                            <i class="material-icons" style="font-size:18px;margin: auto 4px;">add_circle_outline</i>
-                        </div>
-                    </div>
-                </div>
-                
-                <div style="height:280px; width:40%">
-                    <img :src ='movie.smallImgSrc' style='max-height:100%; max-width:100%' class="img-box">
-                </div>
-            </div>
+          <div style="height:280px; width:40%">
+            <img :src="movie.smallImgSrc" style="max-height:100%; max-width:100%" class="img-box" />
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import _ from 'lodash';
 
 export default {
   name: 'Films',
   props: {
-      movie: {
-        type: Object,
-        required: true
-      },
-      
-      play: {
-          type: Boolean,
-          required: true
-      },
+    movie: {
+      type: Object,
+      required: true
+    },
+
+    play: {
+      type: Boolean,
+      required: true
+    }
   },
   data() {
-      return {
-            selectedTab: "Films",
-            genre: '',
-            runtime:'',
-            homepage: '',
-            BASE_YOUTUBE_TRAILER_URL: 'https://www.youtube.com/embed/',
-            API_KEY: "ab5a7db6d911744f7e4e910d9eb5582c",
-            BASE_URL: "https://api.themoviedb.org/3/movie/",
-            trailerPath: '',
-            tabs: [
-                "Home",
-                "Films",
-                "Shows",
-                "Music"
-            ],
-      };
+    return {
+      selectedTab: 'Films',
+      genre: '',
+      runtime: '',
+      homepage: '',
+      BASE_YOUTUBE_TRAILER_URL: 'https://www.youtube.com/embed/',
+      API_KEY: 'ab5a7db6d911744f7e4e910d9eb5582c',
+      BASE_URL: 'https://api.themoviedb.org/3/movie/',
+      trailerPath: '',
+      tabs: ['Home', 'Films', 'Shows', 'Music']
+    };
   },
   methods: {
-      toggleFav() {
-          this.$emit('toggleFavorite',this.movie.id);
-          this.$forceUpdate();
-      },
+    toggleFav() {
+      this.$emit('toggleFavorite', this.movie.id);
+      this.$forceUpdate();
+    },
 
-      handleTab(tab) {
-        this.selectedTab=tab;
-        if (tab=="Home") {
-            this.goHome();
-        }
-      },
-
-      fetchYoutubeURL() {
-          this.$emit('togglePlay');
-          axios.get(this.BASE_URL+this.movie.id+'/videos', {
-            params : {
-                api_key : this.API_KEY,
-                language : "en-US",
-            }
-
-        }).then(response => {
-            console.log(response.data.results[0].key);
-            this.trailerPath = this.BASE_YOUTUBE_TRAILER_URL+response.data.results[0].key;
-        });
-      },
-
-      goHome() {
-          this.$emit('goHome');
+    handleTab(tab) {
+      this.selectedTab = tab;
+      if (tab == 'Home') {
+        this.goHome();
       }
+    },
+
+    fetchYoutubeURL() {
+      this.$emit('togglePlay');
+      axios
+        .get(this.BASE_URL + this.movie.id + '/videos', {
+          params: {
+            api_key: this.API_KEY,
+            language: 'en-US'
+          }
+        })
+        .then(response => {
+          this.trailerPath =
+            this.BASE_YOUTUBE_TRAILER_URL + response.data.results[0].key;
+        });
+    },
+
+    goHome() {
+      this.$emit('goHome');
+    }
   },
 
   updated() {
-        const API_KEY = "ab5a7db6d911744f7e4e910d9eb5582c";
-        const BASE_URL = "https://api.themoviedb.org/3/movie/";
-        const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+    const API_KEY = 'ab5a7db6d911744f7e4e910d9eb5582c';
+    const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 
-        axios.get(BASE_URL+this.movie.id, {
-            params : {
-                api_key : API_KEY,
-                language : "en-US",
-            }
-
-        }).then(response => {
-            console.log(response);
-            let x = [];
-            let duration = response.data.runtime;
-            let hour = Math.floor(duration / 60);
-            let min = duration % 60;
-            this.runtime = hour + 'hr ' + min + 'min'; 
-            this.homepage = response.data.homepage;
-            _.forEach(response.data.genres, (genre, index) => {
-                x.push(genre.name);
-                if(index>1) {
-                    return false;
-                }
-            })
-            this.genre = x.join(',');
+    axios
+      .get(BASE_URL + this.movie.id, {
+        params: {
+          api_key: API_KEY,
+          language: 'en-US'
+        }
+      })
+      .then(response => {
+        let x = [];
+        let duration = response.data.runtime;
+        let hour = Math.floor(duration / 60);
+        let min = duration % 60;
+        this.runtime = hour + 'hr ' + min + 'min';
+        this.homepage = response.data.homepage;
+        _.forEach(response.data.genres, (genre, index) => {
+          x.push(genre.name);
+          if (index > 1) {
+            return false;
+          }
         });
-   },
+        this.genre = x.join(',');
+      });
+  },
 
-   created() {
-       const API_KEY = "ab5a7db6d911744f7e4e910d9eb5582c";
-        const BASE_URL = "https://api.themoviedb.org/3/movie/";
-        const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+  created() {
+    const API_KEY = 'ab5a7db6d911744f7e4e910d9eb5582c';
+    const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 
-        axios.get(BASE_URL+this.movie.id, {
-            params : {
-                api_key : API_KEY,
-                language : "en-US",
-            }
-
-        }).then(response => {
-            console.log(response);
-            let x = [];
-            let duration = response.data.runtime;
-            let hour = Math.floor(duration / 60);
-            let min = duration % 60;
-            this.runtime = hour + 'hr ' + min + 'min'; 
-            this.homepage = response.data.homepage;
-            _.forEach(response.data.genres, (genre, index) => {
-                x.push(genre.name);
-                if(index>1) {
-                    return false;
-                }
-            })
-            this.genre = x.join(',');
-        });``
-   }
-}
+    axios
+      .get(BASE_URL + this.movie.id, {
+        params: {
+          api_key: API_KEY,
+          language: 'en-US'
+        }
+      })
+      .then(response => {
+        let x = [];
+        let duration = response.data.runtime;
+        let hour = Math.floor(duration / 60);
+        let min = duration % 60;
+        this.runtime = hour + 'hr ' + min + 'min';
+        this.homepage = response.data.homepage;
+        _.forEach(response.data.genres, (genre, index) => {
+          x.push(genre.name);
+          if (index > 1) {
+            return false;
+          }
+        });
+        this.genre = x.join(',');
+      });
+    '';
+  }
+};
 </script>
 
 <style>
-    html, body {
-        margin: 0px;
-        height:100%;
-    }
+html,
+body {
+  margin: 0px;
+  height: 100%;
+}
 </style>
 
 <style scoped>
+.col {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+}
 
-    .col {
-        display: flex;
-        flex-direction: column;
-        height:100%;
-        justify-content:space-between;
-    }
+.header {
+  display: flex;
+  width: 100%;
+  padding: 6px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  box-sizing: border-box;
+}
 
-    .header {
-        display: flex;
-        width:100%;
-        padding: 6px;
-        justify-content: flex-start;
-        align-items: flex-start; 
-        box-sizing: border-box;
-    }
+.content {
+  height: 100%;
+  width: 100%;
+  background-image: radial-gradient(
+    circle at 20% 50%,
+    rgba(16.47%, 15.29%, 14.51%, 0.5) 0%,
+    rgba(16.47%, 15.29%, 14.51%, 0.88) 100%
+  );
+}
 
-    .content {
-        height:100%;
-        width:100%;
-        background-image: radial-gradient(circle at 20% 50%, rgba(16.47%, 15.29%, 14.51%, 0.5) 0%, rgba(16.47%, 15.29%, 14.51%, 0.88) 100%)
-    }
+.films-bg {
+  position: relative;
+  background-repeat: no-repeat;
+  background-size: cover;
+  /* Important for applying gradient to background images*/
+  background-color: rgba(0, 0, 0, 0.7);
+  background-blend-mode: multiply;
+  width: 100%;
+  height: 100%;
+  padding: 0px;
+}
 
-    .films-bg {
-        position: relative;
-        background-repeat: no-repeat;
-        background-size:cover;
-        /* Important for applying gradient to background images*/
-        background-color: rgba(0, 0, 0, 0.7);
-        background-blend-mode: multiply;
-        width:100%;
-        height: %;
-        padding:0px;
-    }
+.nav {
+  display: flex;
+  color: white;
+  flex-grow: 3;
+  margin: auto 0;
+}
 
-    .nav {
-        display:flex;
-        color: white;
-        flex-grow:3;
-        margin: auto 0;
-    }
+.nav div {
+  padding: 4px 16px;
+  cursor: pointer;
+}
 
-    .nav div {
-        padding: 4px 16px;
-        cursor: pointer;
-    }
+.nav .selected {
+  background: white;
+  color: black;
+  border-radius: 500px;
+}
 
-    .nav .selected {
-        background: white;
-        color: black;
-        border-radius:500px;
-    }
+.fav {
+  color: white;
+  margin: auto 40px;
+}
 
-    .fav {
-        color: white;
-        margin: auto 40px;
-    }
+.favorite {
+  box-shadow: 0px 0px 70px #cc8e33;
+}
 
-    .favorite {
-        box-shadow: 0px 0px 70px #cc8e33;
-    }
+.fixed-line {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-height: 6em;
+}
 
-    .fixed-line {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        max-height: 6em;
-    }
+.iframe-loader {
+  background: url(../assets/images/loading.gif) center center no-repeat;
+  background-size: 85px;
+  width: 100%;
+  height: 100%;
+}
 
-    .iframe-loader {
-        background:url(../assets/images/loading.gif) center center no-repeat;
-        background-size: 85px;
-        width:100%;
-        height:100%;
-    }
+.img-box {
+  box-shadow: 0px 0px 100px 20px #000000;
+}
 
-    .img-box {
-        box-shadow : 0px 0px 100px 20px #000000;
-    }
+#title:hover {
+  cursor: pointer;
+  color: #0cbbff !important;
+}
 
-    #title:hover {
-        cursor: pointer;
-        color: #0CBBFF !important;
-    }
-
-    a {
-        color: white;
-        text-decoration: none;
-    }
+a {
+  color: white;
+  text-decoration: none;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,124 +1,143 @@
 <template>
   <div class="col">
-      <div class="header" v-if="movieId==''">
-          <span style="color:#FF0000;font-size:64px;">VUEFLIX</span>
-          <span style="color:#fff; font-size:20px;" v-html="tagline"></span>
+    <div class="header" v-if="movieId==''">
+      <span style="color:#FF0000;font-size:64px;">VUEFLIX</span>
+      <span style="color:#fff; font-size:20px;" v-html="tagline"></span>
+    </div>
+    <films
+      class="header"
+      v-else
+      :movie="movies[movieId]"
+      :play="play"
+      @togglePlay="play=!play"
+      @toggleFavorite="toggleFavorite"
+      @goHome="movieId=''"
+    ></films>
+    <section id="footer">
+      <div style="display: flex;">
+        <div v-if="loading" style="color:white;font-size:24px">Loading...</div>
+        <div
+          v-else
+          v-for="movie in movies"
+          v-bind:key="movie.id"
+          v-on:click="handleMovieClick(movie.id)"
+          style="padding:5px 20px; position: relative"
+        >
+          <img :src="movie.smallImgSrc" alt="placeholder" style="width: 100%; height: 100%;" />
+          <i
+            v-if="movie.favorite"
+            class="material-icons"
+            style="position:absolute; right:10%; top: 2px; color:yellow; font-size:18px"
+          >check_circle</i>
+        </div>
       </div>
-      <films class= "header" v-else :movie="movies[movieId]" :play="play" @togglePlay="play=!play" @toggleFavorite="toggleFavorite" @goHome = "movieId=''"></films>
-      <section id="footer">
-          <div style="display: flex;">
-            <div v-if="loading" style="color:white;font-size:24px">Loading...</div>
-            <div v-else v-for="movie in movies" v-on:click="handleMovieClick(movie.id)" style="padding:5px 20px; position: relative">
-                <img :src="movie.smallImgSrc" alt="placeholder" style="width: 100%; height: 100%;">
-                <i v-if="movie.favorite" class="material-icons" style="position:absolute; right:10%; top: 2px; color:yellow; font-size:18px" >check_circle</i>
-            </div>
-            </div>
-      </section>
+    </section>
   </div>
 </template>
 
 <script>
-import Films from './Films'
-import axios from 'axios'
-import _ from 'lodash'
+import Films from './Films';
+import axios from 'axios';
+import _ from 'lodash';
 
 export default {
   name: 'Home',
   components: {
-      Films
+    Films
   },
   data: () => ({
-      movieId: "",
-      play:false,
-      loading: true,
-      tagline: "What's special? Well, Please Dive in to FLIX <i class='em-svg em-wink'></i>",
-      movies: {},
+    movieId: '',
+    play: false,
+    loading: true,
+    tagline:
+      'What\'s special? Well, Please Dive in to FLIX <i class=\'em-svg em-wink\'></i>',
+    movies: {}
   }),
-  
-  created () {
-        const API_KEY = "ab5a7db6d911744f7e4e910d9eb5582c";
-        const BASE_URL = "https://api.themoviedb.org/3/discover/movie";
-        const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
-        const BASE_BACKDROP_IMAGE = "https://image.tmdb.org/t/p/w1400_and_h450_face";
-        const BASE_YOUTUBE_TRAILER_URL = "https://www.youtube.com/watch?v=GTJr8OvyEVQ";
 
-        axios.get(BASE_URL, {
-            params : {
-                api_key : API_KEY,
-                language : "en-US",
-                sort_by: "popularity.desc",
-                include_adult: false,
-                page: 1
-            }
+  created() {
+    const API_KEY = 'ab5a7db6d911744f7e4e910d9eb5582c';
+    const BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
+    const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
+    const BASE_BACKDROP_IMAGE =
+      'https://image.tmdb.org/t/p/w1400_and_h450_face';
 
-        }).then(response => {
-            _.forEach(response.data.results, (movie, index) => {
-                this.movies[movie.id] = {
-                    id: movie.id,
-                    title: movie.title,
-                    description: movie.overview,
-                    largeImgSrc: BASE_BACKDROP_IMAGE+movie.backdrop_path,
-                    smallImgSrc: BASE_IMAGE_URL+movie.poster_path,
-                    releaseDate: movie.release_date,
-                    genre: movie.genre_ids,
-                    favorite: false
-                };
-                if (index > 5) 
-                    return false;
-            });
-                this.loading = false;
+    axios
+      .get(BASE_URL, {
+        params: {
+          api_key: API_KEY,
+          language: 'en-US',
+          sort_by: 'popularity.desc',
+          include_adult: false,
+          page: 1
+        }
+      })
+      .then(response => {
+        _.forEach(response.data.results, (movie, index) => {
+          this.movies[movie.id] = {
+            id: movie.id,
+            title: movie.title,
+            description: movie.overview,
+            largeImgSrc: BASE_BACKDROP_IMAGE + movie.backdrop_path,
+            smallImgSrc: BASE_IMAGE_URL + movie.poster_path,
+            releaseDate: movie.release_date,
+            genre: movie.genre_ids,
+            favorite: false
+          };
+          if (index > 5) return false;
         });
+        this.loading = false;
+      });
   },
   methods: {
-      toggleFavorite(movieId) {
-          this.movies[movieId].favorite  = !this.movies[movieId].favorite;
-          this.$forceUpdate();
-      },
+    toggleFavorite(movieId) {
+      this.movies[movieId].favorite = !this.movies[movieId].favorite;
+      this.$forceUpdate();
+    },
 
-      handleMovieClick(movieId) {
-          this.movieId = movieId;
-          this.play = false;
-      }
+    handleMovieClick(movieId) {
+      this.movieId = movieId;
+      this.play = false;
+    }
   }
-}
+};
 </script>
 
 <style>
-    html, body {
-        margin: 0px;
-        height:100%;
-        padding: 40px 100px;
-        box-sizing: border-box;
-        background: linear-gradient(to bottom right, #7f3737, #2a2122);
-    }
+html,
+body {
+  margin: 0px;
+  height: 100%;
+  padding: 40px 100px;
+  box-sizing: border-box;
+  background: linear-gradient(to bottom right, #7f3737, #2a2122);
+}
 </style>
 
 <style scoped>
+.col {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+  box-shadow: 0px 0px 100px #000;
+}
 
-    .col {
-        display: flex;
-        flex-direction: column;
-        height:100%;
-        justify-content:space-between;
-        box-shadow: 0px 0px 100px #000;
-    }
+#footer {
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #a3081e;
+  z-index: 2;
+}
 
-    #footer {
-        display:flex;
-        flex-direction: column;
-        height: 300px;
-        justify-content:space-around; 
-        align-items:center;       
-        background-color: #A3081E;
-        z-index:2;
-    }
-
-    .header {
-        display:flex;
-        height: 100%;
-        flex-direction: column;
-        background-color:#1F1D1D;
-        align-items:center;
-        justify-content:center;
-    }
+.header {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  background-color: #1f1d1d;
+  align-items: center;
+  justify-content: center;
+}
 </style>
